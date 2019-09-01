ef6e60a5f7e17cd81897a5cb8c6f14fd9442641d
Brian Sakurada <sakuradabs@gmail.com>
2019-09-01 10:27:35 +0900

    Have the models up and running. Successfully added an item to my database.

--- src/inventory/models.py
+++ src/inventory/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Item(models.Model):
     title = models.CharField(max_length=120)
-    cost = models.DecimalField(decimal_places=2)
-    price = models.DecimalField(decimal_places=2)
+    cost = models.DecimalField(decimal_places=2, max_digits=10)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
     category1 = models.CharField(max_length=120)
-    category2 = models.CharField(max_length=120)
-    brand = models.CharField(max_length=120)
+    category2 = models.CharField(blank=True, max_length=120)
+    brand = models.CharField(blank=True, max_length=120)
     description = models.TextField()
     condition = models.CharField(max_length=120)
     weight = models.IntegerField()
@@ -15,9 +15,8 @@ class Item(models.Model):
     height = models.IntegerField()
     material = models.CharField(max_length=120)
 
-
-class Manga(Item):
-    volume = models.CharField(max_length=10)
-    print_date = models.DateField()
-    publish_date = models.DateField()
-    author = models.CharField(max_length=120)
+    # Manga specific fields.
+    volume = models.CharField(blank=True, max_length=10)
+    print_date = models.DateField(blank=True, null=True)
+    publish_date = models.DateField(blank=True, null=True)
+    author = models.CharField(blank=True, max_length=120)
